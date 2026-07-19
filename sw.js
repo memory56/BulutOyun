1aace69e794628753a3cfbabf0fe106a99c32ca8
memory56 <31474094+memory56@users.noreply.github.com>
2026-07-19 14:44:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "bulutoyun-v1";
-const ASSETS = ["./BulutOyun3D_1.html", "./manifest.json", "./icon.svg"];
+const CACHE = "bulutoyun-v2";
+const ASSETS = ["./BulutOyunTD.html", "./manifest.json", "./icon.svg", "./three.min.js"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener("fetch", (e) => { e.respondWith(fetch(e.request).then((res) => { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); return res; }).catch(() => caches.match(e.request))); });
+self.addEventListener("fetch", (e) => { e.respondWith(fetch(e.request).then((res) => { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); return res; }).catch(() => caches.match(e.request).then((m) => m || caches.match("./BulutOyunTD.html")))); });

d753f3e47c45cfebd78b655d7b53e1338c32c11e
memory56 <31474094+memory56@users.noreply.github.com>
2026-07-22 12:49:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "bulutoyun-v14";
-const ASSETS = ["./BulutOyunTD.html", "./manifest.json", "./icon.svg", "./three.min.js"];
+const CACHE = "bulutoyun-v15";
+const ASSETS = ["./BulutOyunTD.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./three.min.js"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", (e) => { e.respondWith(fetch(e.request).then((res) => { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); return res; }).catch(() => caches.match(e.request).then((m) => m || caches.match("./BulutOyunTD.html")))); });
